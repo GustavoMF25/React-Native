e6a253e8356db8c2d25e186053daeeb219305f59
Gustavo Fernandes <gustavomarcelinofernandes25@gmail.com>
2021-05-18 22:10:36 -0300

    Primeiras Mudanças, animação

---
+++ AnimatedStroke.tsx
@@ -0,0 +1,19 @@
+// import React, { useState } from 'react';
+// import Path from 'react-native-svg';
+
+
+// interface AnimatedStrokeProps {
+//     d: String;
+
+// }
+
+// const AnimatedPath = Animated.crea
+
+// const AnimatedStroke = ({ d }: AnimatedStrokeProps) => {
+
+//     const [length, setLength] = useState(0);
+
+//     return (<Path ref={ref} d={d} stroke='black' strokeWidth={10} />);
+// }
+
+// export default AnimatedStroke;
--- App.js
+++ App.js
@@ -1,12 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, View, StatusBar } from 'react-native';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  Easing,
+  interpolate,
+  Extrapolate
+
+} from 'react-native-reanimated';
+
+import Loading from './src/Loading';
+
+import delivery from './src/icon/delivery.png'
 
 export default function App() {
+  const titlePosition = useSharedValue(-60);
+  const imagePosition = useSharedValue(-60)
+
+  useEffect(() => {
+    titlePosition.value = withTiming(0, {
+      duration: 1000,
+      easing: Easing.bounce,
+    },
+      () => {
+        imagePosition.value = withTiming(0, {
+          duration: 2000,
+          easing: Easing.out(Easing.exp),
+        });
+      },
+    );
+  }, [])
+
+  const titleStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: titlePosition.value }],
+      opacity: interpolate(
+        titlePosition.value,
+        [30, 0],
+        [0, 1],
+        Extrapolate.CLAMP,
+
+      ),
+    };
+  });
+
+  const deliveryStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: imagePosition.value }],
+    }
+  })
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar barStyle="light-content" backgroundColor="#13131A" />
+      <Animated.Text style={[styles.title, titleStyle]}>
+        Loading ...
+      </Animated.Text>
+      <Animated.Image style={[styles.delivery, deliveryStyle]} source={delivery} />
+
+      <Loading />
     </View>
   );
 }
@@ -14,8 +68,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fafafa',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 32,
+    color: '#030303'
+  },
+  delivery: {
+    width: 100,
+    height: 100,
+
+  }
 });
